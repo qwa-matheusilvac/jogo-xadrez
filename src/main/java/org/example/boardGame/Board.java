fa7f6cbdf3e621a7a0e7b4ccd2a59e245357e94f
qwa-matheusilvac <matheus.correia@qwasolucoes.com.br>
2025-04-04 11:32:09 -0300

    feat: BoardException

--- src/main/java/org/example/boardGame/Board.java
+++ src/main/java/org/example/boardGame/Board.java
@@ -7,31 +7,57 @@ import lombok.Setter;
 public class Board {
 
     @Getter
-    @Setter
     private int rows;
 
     @Getter
-    @Setter
     private int columns;
 
     private  Piece[][] pieces;
 
     public Board(int rows, int columns) {
-        this.rows = rows;
-        this.columns = columns;
-        pieces = new Piece[rows][columns];
+        if (rows <= 1 || columns <= 1) {
+            throw new BoardException("Rows and columns must be greater than 1");
+        } else {
+            this.rows = rows;
+            this.columns = columns;
+            pieces = new Piece[rows][columns];
+        }
     }
 
     public Piece getPiece(int row, int column) {
+        if(!positionExists(row, column)){
+            throw new BoardException("Position does not exist");
+        }
         return pieces[row][column];
     }
 
     public Piece piece(Position position){
+        if(!positionExists(position)){
+            throw new BoardException("Position does not exist");
+        }
         return pieces[position.getRow()][position.getColumn()];
     }
 
     public void placePiece(Piece piece, Position position) {
+        if(thereIsAPiece(position)){
+            throw new BoardException("Position already occupied");
+        }
         pieces[position.getRow()][position.getColumn()] = piece;
         piece.position = position;
+    }
+
+    private boolean positionExists(int row, int column){
+        return row >= 0 && row < rows && column >= 0 && column < columns;
+    }
+
+    public boolean positionExists(Position position) {
+        return positionExists(position.getRow(), position.getColumn());
+    }
+
+    public boolean thereIsAPiece(Position position) {
+        if(!positionExists(position)){
+            throw new BoardException("Position does not exist");
+        }
+       return piece(position) != null;
     }
 }
---
+++ src/main/java/org/example/boardGame/BoardException.java
@@ -0,0 +1,7 @@
+package org.example.boardGame;
+
+public class BoardException extends RuntimeException {
+    public BoardException(String message) {
+        super(message);
+    }
+}
