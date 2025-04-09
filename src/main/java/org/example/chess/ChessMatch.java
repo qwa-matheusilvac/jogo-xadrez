26294ee221defed52f11dffd6ea747348386d862
qwa-matheusilvac <matheus.correia@qwasolucoes.com.br>
2025-04-09 11:04:08 -0300

    feat: Knight

--- src/main/java/org/example/chess/ChessMatch.java
+++ src/main/java/org/example/chess/ChessMatch.java
@@ -4,10 +4,7 @@ import lombok.Getter;
 import org.example.boardGame.Board;
 import org.example.boardGame.Piece;
 import org.example.boardGame.Position;
-import org.example.chess.pieces.Bishop;
-import org.example.chess.pieces.King;
-import org.example.chess.pieces.Pawn;
-import org.example.chess.pieces.Rook;
+import org.example.chess.pieces.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -183,9 +180,11 @@ public class ChessMatch{
 
     private void initialSetup(){
         placeNewPieces('a', 1, new Rook(board, Color.WHITE));
+        placeNewPieces('b', 1, new Knight(board, Color.WHITE));
         placeNewPieces('c', 1, new Bishop(board, Color.WHITE));
         placeNewPieces('e', 1, new King(board, Color.WHITE));
         placeNewPieces('f', 1, new Bishop(board, Color.WHITE));
+        placeNewPieces('g', 1, new Knight(board, Color.WHITE));
         placeNewPieces('h', 1, new Rook(board, Color.WHITE));
         placeNewPieces('a', 2, new Pawn(board, Color.WHITE));
         placeNewPieces('b', 2, new Pawn(board, Color.WHITE));
@@ -197,9 +196,11 @@ public class ChessMatch{
         placeNewPieces('h', 2, new Pawn(board, Color.WHITE));
 
         placeNewPieces('a', 8, new Rook(board, Color.BLACK));
+        placeNewPieces('b', 8, new Knight(board, Color.BLACK));
         placeNewPieces('c', 8, new Bishop(board, Color.BLACK));
         placeNewPieces('e', 8, new King(board, Color.BLACK));
         placeNewPieces('f', 8, new Bishop(board, Color.BLACK));
+        placeNewPieces('g', 8, new Knight(board, Color.BLACK));
         placeNewPieces('h', 8, new Rook(board, Color.BLACK));
         placeNewPieces('a', 7, new Pawn(board, Color.BLACK));
         placeNewPieces('b', 7, new Pawn(board, Color.BLACK));
